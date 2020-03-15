f0d3b744d5199651f35f51c4f830cf258d93d03a
mperdeck@gmail.com <mperdeck@gmail.com>
2020-03-15 23:12:55 +1100

    add application insights

--- WebJobsSDKSample/Program.cs
+++ WebJobsSDKSample/Program.cs
@@ -9,17 +9,23 @@ namespace WebJobsSDKSample
         static void Main(string[] args)
         {
             var builder = new HostBuilder();
+            builder.UseEnvironment(EnvironmentName.Development);
             builder.ConfigureWebJobs(b =>
             {
                 b.AddAzureStorageCoreServices();
                 b.AddAzureStorage();
             });
-
             builder.ConfigureLogging((context, b) =>
             {
                 b.AddConsole();
-            });
 
+                // If the key exists in settings, use it to enable Application Insights.
+                string instrumentationKey = context.Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"];
+                if (!string.IsNullOrEmpty(instrumentationKey))
+                {
+                    b.AddApplicationInsightsWebJobs(o => o.InstrumentationKey = instrumentationKey);
+                }
+            });
             var host = builder.Build();
             using (host)
             {
